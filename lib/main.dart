b77a8ac43d92f74affa1fcaf01f7f2adb685ce16
Bouziane mohammed amin <amineboos36@gmail.com>
2020-07-29 10:52:36 +0100

    Bloc : Flutter Counter example

--- lib/main.dart
+++ lib/main.dart
@@ -1,18 +1,18 @@
 import 'package:flutter/material.dart';
-import 'package:learnflutter/inherited_widget/shopingcard.dart';
-import 'package:learnflutter/screens/home.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:learnflutter/counter_bloc.dart';
+import 'package:learnflutter/counter_page.dart';
 
-main() => runApp(MyApp());
+void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
-  const MyApp({Key key}) : super(key: key);
-
   @override
   Widget build(BuildContext context) {
-    return ShoppingCard(
-      child: MaterialApp(
-        debugShowCheckedModeBanner: false,
-        home: Home(),
+    return MaterialApp(
+      title: 'Flutter Demo',
+      home: BlocProvider<CounterBloc>(
+        create: (context) => CounterBloc(),
+        child: CounterPage(),
       ),
     );
   }
--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -1,76 +1,27 @@
-import 'dart:math';
-
-import 'package:badges/badges.dart';
 import 'package:flutter/material.dart';
-import 'package:learnflutter/inherited_widget/shopingcard.dart';
-import 'package:learnflutter/models/product.dart';
-import 'package:learnflutter/screens/card_details.dart';
 
 class Home extends StatelessWidget {
-  List<Product> prodects = List.generate(
-      100,
-      (index) => Product(
-            isCheck: false,
-            name: 'Product $index',
-            price: Random().nextInt(5000),
-          ));
+  const Home({Key key}) : super(key: key);
+
   @override
   Widget build(BuildContext context) {
-    ShoppingCard card = ShoppingCard.of(context);
-    return StatefulBuilder(builder: (context, StateSetter setState) {
-      return Scaffold(
-        appBar: AppBar(
-          title: Text('Shopping Cart '),
-          actions: <Widget>[
-            Container(
-              margin: EdgeInsets.all(15),
-              child: Badge(
-                elevation: 10,
-                badgeContent: Text(
-                  '${card.products.length}',
-                  style: TextStyle(
-                    color: Colors.white,
-                  ),
-                ),
-                child: IconButton(
-                    icon: Icon(Icons.shopping_cart),
-                    onPressed: () {
-                      Navigator.push(context,
-                          MaterialPageRoute(builder: (context) {
-                        return CardDetails();
-                      }));
-                    }),
+    return Scaffold(
+      appBar: AppBar(),
+      body: Center(
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          children: <Widget>[
+            IconButton(icon: Icon(Icons.remove), onPressed: () {}),
+            Text(
+              '5',
+              style: TextStyle(
+                fontSize: 20,
               ),
-            )
+            ),
+            IconButton(icon: Icon(Icons.add), onPressed: () {}),
           ],
         ),
-        body: ListView.builder(
-            itemCount: prodects.length,
-            itemBuilder: (BuildContext context, indexd) {
-              return ListTile(
-                subtitle: Text('${prodects[indexd].price} DA'),
-                title: Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                  children: <Widget>[
-                    Text(prodects[indexd].name),
-                    Checkbox(
-                        value: prodects[indexd].isCheck,
-                        onChanged: (value) {
-                          setState(() {
-                            prodects[indexd].isCheck =
-                                !prodects[indexd].isCheck;
-                            if (prodects[indexd].isCheck) {
-                              card.products.add(prodects[indexd]);
-                            } else {
-                              card.products.remove(prodects[indexd]);
-                            }
-                          });
-                        }),
-                  ],
-                ),
-              );
-            }),
-      );
-    });
+      ),
+    );
   }
 }
